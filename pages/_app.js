aba5b59e8a4966d7ce12ea2848e11db4fb6e9e02
ronalking182 <ronalking2001@icloud.com>
2022-04-13 21:31:19 +0100

    google analytic

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
 import '../styles/globals.css'
 
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-CPJRZ6NMFY"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-CPJRZ6NMFY');
+// </script>
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return<>
+     <script
+     strategy="lazyOnload" 
+     src="https://www.googletagmanager.com/gtag/js?id=G-CPJRZ6NMFY"
+     />
+     <script 
+     strategy="lazyOnload" 
+     >
+       {
+         `
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+       
+         gtag('config', 'G-CPJRZ6NMFY');
+         `
+       }
+     </script>
+   <Component {...pageProps} />
+   </>
 }
 
 export default MyApp
