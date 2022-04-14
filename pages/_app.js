0f771f6d895263909f481bd67152c29e1fb2d964
ronalking182 <ronalking2001@icloud.com>
2022-04-14 01:25:13 +0100

    analytics one

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,52 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
+
+// import Script from 'next/script'
 
 // <!-- Global site tag (gtag.js) - Google Analytics -->
 // <script async src="https://www.googletagmanager.com/gtag/js?id=G-CPJRZ6NMFY"></script>
@@ -9,29 +57,3 @@ import '../styles/globals.css'
 
 //   gtag('config', 'G-CPJRZ6NMFY');
 // </script>
-
-
-function MyApp({ Component, pageProps }) {
-  return<>
-     <script
-     strategy="lazyOnload" 
-     src="https://www.googletagmanager.com/gtag/js?id=G-CPJRZ6NMFY"
-     />
-     <script 
-     strategy="lazyOnload" 
-     >
-       {
-         `
-         window.dataLayer = window.dataLayer || [];
-         function gtag(){dataLayer.push(arguments);}
-         gtag('js', new Date());
-       
-         gtag('config', 'G-CPJRZ6NMFY');
-         `
-       }
-     </script>
-   <Component {...pageProps} />
-   </>
-}
-
-export default MyApp
